beae5aec06be80c568cd4268e5f5a3162d4890fb
Tony Davis <tony@tessive.com>
2014-06-23 20:37:37 -0600

    Updated header, added license.

--- Contents.m
+++ Contents.m
@@ -1,4 +1,4 @@
-% SOURCE
+% RViewer Contents
 %
 % Files
 %  Base classes
@@ -13,3 +13,16 @@
 %   userviewer  - A cell-mode script showing how to use RViewer 
 %   rviewerdemo - A GUIDE generated figure and code using RViewer 
 %   testrviewer - Test Rviewer
+%
+%  Copyright 2014 Tessive LLC
+%    Licensed under the Apache License, Version 2.0 (the "License");
+%    you may not use this file except in compliance with the License.
+%    You may obtain a copy of the License at
+% 
+%      http://www.apache.org/licenses/LICENSE-2.0
+% 
+%    Unless required by applicable law or agreed to in writing, software
+%    distributed under the License is distributed on an "AS IS" BASIS,
+%    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%    See the License for the specific language governing permissions and
+%    limitations under the License.
